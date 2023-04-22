a0a17ddf8b05ec26416aa50535db77eb0360f7b3
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-22 20:16:56 +0800

    Pseudocode

---
+++ Even_And_Odd_Numbers_TextFile.py
@@ -0,0 +1,11 @@
+# Kristine Joy Barrina # BSCPE 1-5 # April 22, 2023
+# Creating a program that reads a file then extract the even and odd numbers then place it in a two different text files.
+
+# PSEUDOCODE
+
+# Open a file called numbers.txt
+# Print all integers inside the file
+# Use for loop with range of 20
+# If number is even, put it in even.txt
+# If number is odd, put it in odd.txt
+# Design output using tkinter
